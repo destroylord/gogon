f2b62cf2421d4c2592f8420b6186bbe391cd23c5
destroylord <masapin68@gmail.com>
2021-12-31 18:55:28 +0700

    slice

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	name := "Dafrin"
 	const tiket int = 50
 	var remainingTickets uint = 50
-	var bookings [50]string
+	bookings := []string{}
 
 	fmt.Printf("Hello World %v\n", name)
 	fmt.Printf("Tiket untuk nonton bola: %v\n", tiket)
@@ -31,15 +31,17 @@ func main() {
 	fmt.Scan(&userTiket)
 
 	remainingTickets = remainingTickets - uint(userTiket)
-	bookings[0] = firstName + " " + lastname
+	bookings = append(bookings, firstName+" "+lastname)
 
-	fmt.Printf("Array: %v\n", bookings)
+	fmt.Printf("Slice: %v\n", bookings)
 	fmt.Printf("First Value: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+	fmt.Printf("Slice type: %T\n", bookings)
+	fmt.Printf("Slice length: %v\n", len(bookings))
 
 	userTiket = 50
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation eamil at %v\n",
 		firstName, lastname, userTiket, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, name)
+
+	fmt.Print("These are all our bookings: %v\n", bookings)
 }
