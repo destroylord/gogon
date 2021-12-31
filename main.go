50eb0c744b11e95ed2d8d7b990c60d396fcd6a36
destroylord <masapin68@gmail.com>
2021-12-31 17:34:30 +0700

    data type

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import "fmt"
 
 func main() {
 
-
 	// refrensi : https://pkg.go.dev/fmt#pkg-overview
-	var name = "Dafrin"
-	const tiket = 50
+	var name string = "Dafrin"
+	const tiket int = 50
 
 	fmt.Printf("Hello World %v\n", name)
-	fmt.Printf("Tiket untuk nonton bola: %v", tiket)
+	fmt.Printf("Tiket untuk nonton bola: %v\n", tiket)
 
+	var userName string
+	var userTiket int
+
+	userName = "Tom"
+	userTiket = 50
+	fmt.Printf("User: %v booked %v.\n", userName, userTiket)
 }
